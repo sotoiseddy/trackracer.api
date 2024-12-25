7cb0ab902713bc4641375fb0e88aae2f720e2ccf
Lom bu <lomn_bu@outlook.com>
2024-12-25 16:27:00 +0530

    .

---
+++ trackracer.api/Interfaces/ITrackingRequestStatus.cs
@@ -0,0 +1,15 @@
+﻿using trackracer.Models.Accounts;
+
+namespace trackracer.api.Interfaces
+{
+    public interface ITrackingRequestStatus
+    {
+      
+        public bool SaveRequest(TrackingRequestStatusModel request);
+        public List<TrackingRequestStatusModel> GetAllRequestStatuses();
+        public TrackingRequestStatusModel GetRequestStatusBySenderID(Guid SenderID);
+        public TrackingRequestStatusModel GetTrackingRequestByReceiverID(Guid ReceiverID);
+       
+
+    }
+}
